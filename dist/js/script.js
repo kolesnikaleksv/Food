5fd4c73dfa5d1c2f6a4a696565284fc34ad098bd
kolesnikaleks <kolesnikaleksv@gmail.com>
2020-06-17 15:52:27 +0300

    forming cards without using classes

--- dist/js/script.js
+++ dist/js/script.js
@@ -2201,8 +2201,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
       element.innerHTML = `
                 <img src="${this.src}" alt="${this.alt}">
-                <h3 class="menu__item-subtitle">Меню “${this.title}”</h3>
-                <div class="menu__item-descr">В меню “${this.title}” ${this.descr}
+                <h3 class="menu__item-subtitle"> ${this.title}</h3>
+                <div class="menu__item-descr"> ${this.descr}
                 </div>
                 <div class="menu__item-divider"></div>
                 <div class="menu__item-price">
@@ -2223,9 +2223,17 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     return await res.json();
-  };
+  }; // getResource('http://localhost:3000/menu')
+  //     .then(data => {
+  //         data.forEach(({img, altimg, title, descr, price}) => {
+  //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+  //         });
+  //     });
 
-  getResource('http://localhost:3000/menu').then(data => {
+
+  getResource('http://localhost:3000/menu').then(data => createCard(data));
+
+  function createCard(data) {
     data.forEach(({
       img,
       altimg,
@@ -2233,9 +2241,24 @@ document.addEventListener('DOMContentLoaded', () => {
       descr,
       price
     }) => {
-      new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+      const element = document.createElement('div');
+      price = price * 27;
+      element.classList.add('menu__item');
+      element.innerHTML = `
+                    <img src="${img}" alt="${altimg}">
+                    <h3 class="menu__item-subtitle"> ${title}</h3>
+                    <div class="menu__item-descr"> ${descr}
+                    </div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${price}</span> грн/день</div>
+                    </div>
+                `;
+      document.querySelector('.menu .container').append(element);
     });
-  }); //Forms
+  } //Forms
+
 
   const forms = document.querySelectorAll('form');
   const message = {
--- src/js/main.js
+++ src/js/main.js
@@ -174,8 +174,8 @@ document.addEventListener('DOMContentLoaded', () => {
             
             element.innerHTML =  `
                 <img src="${this.src}" alt="${this.alt}">
-                <h3 class="menu__item-subtitle">Меню “${this.title}”</h3>
-                <div class="menu__item-descr">В меню “${this.title}” ${this.descr}
+                <h3 class="menu__item-subtitle"> ${this.title}</h3>
+                <div class="menu__item-descr"> ${this.descr}
                 </div>
                 <div class="menu__item-divider"></div>
                 <div class="menu__item-price">
@@ -197,14 +197,37 @@ document.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-    getResource('http://localhost:3000/menu')
-        .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
-                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
-            });
-        });
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    //         data.forEach(({img, altimg, title, descr, price}) => {
+    //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    //         });
+    //     });
 
-    
+    getResource('http://localhost:3000/menu')
+        .then(data => createCard(data));
+        
+        function createCard(data) {
+            data.forEach(({img, altimg, title, descr, price}) => {
+                const element = document.createElement('div');
+                price = price * 27;
+                element.classList.add('menu__item');
+
+                element.innerHTML = `
+                    <img src="${img}" alt="${altimg}">
+                    <h3 class="menu__item-subtitle"> ${title}</h3>
+                    <div class="menu__item-descr"> ${descr}
+                    </div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${price}</span> грн/день</div>
+                    </div>
+                `;
+
+                document.querySelector('.menu .container').append(element);
+            });
+        }
 
         //Forms
     const forms = document.querySelectorAll('form');
